d7ac9a42ca0b2dc5923186568edb75a92d9508f0
IvanYakimov <ivan.yakimov@atom.team>
2025-07-29 11:25:26 +0700

    Implement basic signal-base timer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <chrono>
 #include <coroutine>
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 #include <functional>
 #include <iostream>
 #include <memory>
@@ -9,52 +13,115 @@
 
 using namespace std::chrono_literals;
 
+using SigVal = union ::sigval;
+using SigEvent = struct ::sigevent;
+using ITimerSpec = struct ::itimerspec;
+
+struct EventLoop;
+
 struct Task {
-  std::chrono::time_point<std::chrono::system_clock> mTimeout;
+  void onNotification();
+
   std::function<void(void)> mCallback;
+  std::weak_ptr<EventLoop> mEventLoop;
+  size_t mIndex;
 };
 
-bool operator<(const Task &lhs, const Task &rhs) {
-  // 0 priority is higher that say 100
-  return lhs.mTimeout > rhs.mTimeout;
+void notificationCallback(union sigval sv) {
+  Task *task = static_cast<Task *>(sv.sival_ptr);
+  if (!task) {
+    return;
+  }
+  task->onNotification();
 }
 
 struct EventLoop : std::enable_shared_from_this<EventLoop> {
-  void onTimeout(std::chrono::system_clock::duration duration,
+
+  void handleTask(size_t index) {
+    auto weak = weak_from_this();
+    auto self = weak.lock();
+    if (!self) {
+      return;
+    }
+    std::lock_guard guard(self->mMutex);
+    auto task = self->mPendingTasks.at(index);
+    self->mReadyTasks.push(std::move(task));
+    self->mPendingTasks.erase(index);
+  }
+
+  void onTimeout(std::chrono::nanoseconds duration,
                  std::function<void(void)> callback) {
-    auto self = weak_from_this().lock();
-    auto until = std::chrono::system_clock::now() + duration;
-    Task task{
-        .mTimeout = until,
+    auto weak = weak_from_this();
+    auto self = weak.lock();
+    if (!self) {
+      return;
+    }
+    std::lock_guard guard(self->mMutex);
+    self->mPendingTasks[mTaskCount] = Task{
         .mCallback = std::move(callback),
+        .mEventLoop = weak_from_this(),
+        .mIndex = mTaskCount,
     };
-    std::lock_guard lg(self->mMutex);
-    self->mTasks.push(std::move(task));
+    SigEvent sigEvent;
+    sigEvent.sigev_notify = ::SIGEV_THREAD;
+    sigEvent.sigev_value.sival_ptr = &self->mPendingTasks.at(mTaskCount);
+    sigEvent.sigev_notify_function = notificationCallback;
+    sigEvent.sigev_notify_attributes = NULL;
+    timer_t timerId;
+    if (timer_create(CLOCK_REALTIME, &sigEvent, &timerId) == -1) {
+      perror("timer_create");
+      exit(EXIT_FAILURE);
+    }
+    ITimerSpec iTimerSpec;
+    iTimerSpec.it_value.tv_sec = duration.count() / 1000'000'000;
+    iTimerSpec.it_value.tv_nsec = duration.count() % 1000'000'000;
+    iTimerSpec.it_interval.tv_sec = 0;
+    iTimerSpec.it_interval.tv_nsec = 0;
+    if (timer_settime(timerId, 0, &iTimerSpec, NULL) == -1) {
+      perror("timer_settime");
+      exit(EXIT_FAILURE);
+    }
+    mTaskCount += 1;
   }
 
   void run() {
-    auto self = weak_from_this().lock();
+    auto weak = weak_from_this();
+    auto self = weak.lock();
+    if (!self) {
+      return;
+    }
     while (true) {
-      // FIXME: use OS capabilities directly to avoid stalling
+      // FIXME: wait on CV capabilities directly to avoid stalling
       std::this_thread::sleep_for(100ms);
       const auto currentTime = std::chrono::system_clock::now();
-      std::lock_guard lg(mMutex);
-      if (self->mTasks.empty()) {
-        return;
+      std::lock_guard guard(self->mMutex);
+      if (self->mReadyTasks.empty()) {
+        if (self->mPendingTasks.empty()) {
+          return;
+        }
+        continue;
       }
-      const auto &top = self->mTasks.top();
-      if (top.mTimeout <= currentTime) {
-        top.mCallback();
-        self->mTasks.pop();
-      }
+      const auto &top = self->mReadyTasks.front();
+      top.mCallback();
+      self->mReadyTasks.pop();
     }
   }
 
+  std::atomic<size_t> mTaskCount{0};
   std::mutex mMutex;
-  std::priority_queue<Task> mTasks;
+  std::unordered_map<size_t, Task> mPendingTasks;
+  std::queue<Task> mReadyTasks;
 
   ~EventLoop() { std::cerr << "EventLoop was destroyed...\n"; }
 };
+
+void Task::onNotification() {
+  auto loop = mEventLoop.lock();
+  if (!loop) {
+    return;
+  }
+  loop->handleTask(mIndex);
+}
 
 struct TimerPromise;
 
